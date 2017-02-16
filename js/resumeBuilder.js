20eb1ced4d7474659a4eb92ab21bfbdcd8995399
muenzaniso <pmuenzaniso@gmail.com>
2017-02-16 17:43:02 +0200

    add replace

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,6 @@
 
 $("#main").append(["James Z Phiri"]);
 
-var awesomeThoughts= 'i am James Z Phiri and i am AWESOME!' 
+var awesomeThoughts= 'i am James Z Phiri and i am AWESOME!'
+// [string].replace([old],([new]))
+var newAwesomeThoughts= awesomeThoughts.replace(["AWESOME"],["FUN"]);
